a6689b8a67c2876cc0c0caa395ac67ba572b1742
Samantha <samanthavillafana8@gmail.com>
2024-11-09 16:31:50 -0500

    complete lab

--- server/app.py
+++ server/app.py
@@ -3,7 +3,6 @@
 from flask import Flask, make_response, jsonify, request, session
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Article, User
 
 app = Flask(__name__)
@@ -18,40 +17,55 @@ db.init_app(app)
 
 api = Api(app)
 
-class ClearSession(Resource):
-
+class Logout(Resource):
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
-
+        session.pop('user_id', None)  
         return {}, 204
 
+class Login(Resource):
+    def post(self):
+        username = request.get_json().get('username')
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            session['user_id'] = user.id
+            return jsonify(user.to_dict())  
+        return {'message': 'User not found'}, 404
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            if user:
+                return jsonify(user.to_dict()) 
+            return {'message': 'User not found'}, 404
+        return {}, 401  
+
+
+
 class IndexArticle(Resource):
-    
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return articles, 200
 
-class ShowArticle(Resource):
 
+class ShowArticle(Resource):
     def get(self, id):
         session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
         session['page_views'] += 1
 
         if session['page_views'] <= 3:
-
             article = Article.query.filter(Article.id == id).first()
-            article_json = jsonify(article.to_dict())
-
-            return make_response(article_json, 200)
+            return make_response(jsonify(article.to_dict()), 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
-api.add_resource(ClearSession, '/clear')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
